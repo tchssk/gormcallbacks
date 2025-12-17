8abe21cc44c51a4821825343a353b089df358f9b
Taichi Sasaki <tchsskk@gmail.com>
2025-12-17 20:12:30 +0900

    Add CountOption to v1

--- v1/count.go
+++ v1/count.go
@@ -13,8 +13,14 @@ type CountConfig struct {
 	UpdateCount map[string]int
 }
 
+type (
+	CountOptions struct {
+	}
+	CountOption func(*CountOptions)
+)
+
 // Count is a callback to count operations per table.
-func Count(m map[string]int) func(*gorm.Scope) {
+func Count(m map[string]int, options *CountOptions) func(*gorm.Scope) {
 	var mu sync.Mutex
 	return func(scope *gorm.Scope) {
 		mu.Lock()
@@ -24,24 +30,28 @@ func Count(m map[string]int) func(*gorm.Scope) {
 }
 
 // RegisterCountCallbacks registers a Count callback for each operation.
-func RegisterCountCallbacks(db *gorm.DB, config *CountConfig) {
+func RegisterCountCallbacks(db *gorm.DB, config *CountConfig, options ...CountOption) {
+	var opts CountOptions
+	for _, option := range options {
+		option(&opts)
+	}
 	createCallback := db.Callback().Create()
 	if config.CreateCount != nil {
-		createCallback.After("gorm:create").Register("count", Count(config.CreateCount))
+		createCallback.After("gorm:create").Register("count", Count(config.CreateCount, &opts))
 	}
 
 	queryCallback := db.Callback().Query()
 	if config.QueryCount != nil {
-		queryCallback.After("query").Register("count", Count(config.QueryCount))
+		queryCallback.After("query").Register("count", Count(config.QueryCount, &opts))
 	}
 
 	deleteCallback := db.Callback().Delete()
 	if config.DeleteCount != nil {
-		deleteCallback.After("gorm:delete").Register("count", Count(config.DeleteCount))
+		deleteCallback.After("gorm:delete").Register("count", Count(config.DeleteCount, &opts))
 	}
 
 	updateCallback := db.Callback().Update()
 	if config.UpdateCount != nil {
-		updateCallback.After("gorm:update").Register("count", Count(config.UpdateCount))
+		updateCallback.After("gorm:update").Register("count", Count(config.UpdateCount, &opts))
 	}
 }
